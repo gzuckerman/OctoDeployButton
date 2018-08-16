6d11f2db48fbad52dbaf53b69b44a6f0201518e7
Gregory Zuckerman <gregory.zuckerman@gmail.com>
2018-08-16 20:59:11 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const octokit = require('@octokit/rest')()
+
+octokit.authenticate({
+  type: 'token',
+  token: process.env.GH_TOKEN
+})
+
+exports.handler = (event, context, callback) => {
+  console.log(`Received event: ${event}`)
+  let tag_name
+  const owner = process.env.GH_LOGIN
+  const repo = process.env.GH_REPO
+
+  octokit.repos.getLatestRelease({
+    owner,
+    repo
+  }).then(result => {
+    tag_name = (parseInt(result.data.tag_name) + 1.0).toString() + '.0'
+
+    octokit.repos.createRelease({
+      owner,
+      repo,
+      tag_name
+    }, (error, result) => {
+      if (error) throw new Error()
+      if (result) console.log(`Created Release: ${JSON.stringify(result)}`)
+    })
+  })
+
+  octokit.repos.createDeployment({
+    owner,
+    repo,
+    ref: 'master',
+    description: `Deploying ${tag_name} version`
+  }, (error, result) => {
+    if (error) throw new Error()
+    if (result) console.log(`Created Deployment: ${JSON.stringify(result)}`)
+  })
+}
